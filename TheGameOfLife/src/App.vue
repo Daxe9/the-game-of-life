9ebd991bdb648b6e982ccec10dccfa54e02049f6
Daxe9 <davidexie694@gmail.com>
2022-03-03 18:55:35 +0100

    add vue version of the game of life

---
+++ TheGameOfLife/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+        <Grid />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Grid from "./components/Grid.vue";
+
+export default {
+  name: 'App',
+
+  components: {
+      Grid
+  },
+}
+</script>
---
+++ TheGameOfLife/src/components/Grid.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "Grid"
+}
+</script>
+
+<style scoped>
+
+</style>
